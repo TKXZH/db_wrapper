26a09ba62a513f512a69d481fa24b572313e7b27
zonghuixu <zonghui.xu@coupang.com>
2018-09-09 20:26:59 +0800

    add test case to test jenkins pipeline

---
+++ src/test/java/top/xvzonghui/SqlExecutorTest.java
@@ -0,0 +1,16 @@
+package top.xvzonghui;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * author:zonghui
+ */
+public class SqlExecutorTest {
+	@Test
+	public void shutDown() throws Exception {
+		assertNotNull(1);
+	}
+
+}
